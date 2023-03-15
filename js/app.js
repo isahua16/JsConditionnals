0750f3a41ae9c7a48dd86855d2cc082481f85d22
isahua16 <isaelhuard@gmail.com>
2023-03-14 19:29:11 -0600

    created a simple conditional statement

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let user_points = 98;
+
+// user_points = 43;
+
+if (user_points >= 75) {
+    console.log("Congratulations, you have won the game!");
+} else {
+    console.log("Better luck next time!");
+}
