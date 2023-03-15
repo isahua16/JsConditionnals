d32354e6cad420c0495b62535c70772966a93158
isahua16 <isaelhuard@gmail.com>
2023-03-14 20:17:46 -0600

    added boolean operators

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-let user_points = 98;
+let user_points = 9;
+let is_member = true;
+let user_name = "Isael";
+// if (user_points >= 75) {
+//     console.log("Congratulations, you have won the game!");
+// } else if (user_points >= 50) {
+//     console.log("You are almost at 75 points!");
+// } else {
+//     console.log("Keep going, you are doing great!");
+// }
 
-user_points = 90;
+// if (is_member === true && user_points >= 90) {
+//     console.log("You ROCK!");
+// } else {
+//     console.log("You are not special...");
+// }
 
-if (user_points >= 75) {
-    console.log("Congratulations, you have won the game!");
-} else if (user_points >= 50) {
-    console.log("You are almost at 75 points!");
+// if (is_member === false || user_points < 10) {
+//     console.log("Please leave");
+// } else {
+//     console.log("Keep going!");
+// }
+
+if (user_name === "Alex") {
+    console.log("Welcome to the class, Alex!");
 } else {
-    console.log("Keep going, you are doing great!");
+    console.log("Welcome to the class!");
 }
-
