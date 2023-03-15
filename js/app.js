cb4e3bf129c4630b446b95e590c31be64cc70089
isahua16 <isaelhuard@gmail.com>
2023-03-14 19:58:08 -0600

    Added an else if conditional

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 let user_points = 98;
 
-// user_points = 43;
+user_points = 90;
 
 if (user_points >= 75) {
     console.log("Congratulations, you have won the game!");
+} else if (user_points >= 50) {
+    console.log("You are almost at 75 points!");
 } else {
-    console.log("Better luck next time!");
+    console.log("Keep going, you are doing great!");
 }
+
